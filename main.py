655db84067b43c7743668d803dd6b4267ae60e61
zdupre18 <126183811+zdupre18@users.noreply.github.com>
2023-11-07 10:27:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,16 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 			work_fn2(n)
 			))
 	return result
+def span_calc(a, W, n):
+def work_function_constant(n):
+    return 1
+
+def work_function_logarithmic(n):
+    return int(math.log(n, 2))
+
+def work_function_linear(n):
+    return n
+
 
 def print_results(results):
 	""" done """
@@ -98,7 +108,21 @@ def print_results(results):
 							floatfmt=".3f",
 							tablefmt="github"))
 
-def test_compare_work():
+def work_fn1(n):
+    # Define the first work function
+    return n**2
+
+def work_fn2(n):
+    # Define the second work function
+    return n * (n-1)
+
+def test_compare_work(n_values):
+    for n in n_values:
+        # Compare work functions using compare_work
+        work1 = work_calc(work_fn1, n)
+        work2 = work_calc(work_fn2, n)
+
+        print(f"n = {n}: Work Function 1 = {work1}, Work Function 2 = {work2}")
 	# curry work_calc to create multiple work
 	# functions taht can be passed to compare_work
     
@@ -107,11 +131,22 @@ def test_compare_work():
 
     res = compare_work(work_fn1, work_fn2)
 	print(res)
+n_values = [1, 10, 100, 1000, 10000]
+test_compare_work(n_values)
 
-def test_compare_span():
-	# TODO
-	
+def test_compare_span(n_values):
+    a = 2  # Number of processors
+
+    for n in n_values:
+        span_1 = span_calc(a, work_function_1, n)
+        span_n_squared = span_calc(a, work_function_n_squared, n)
+        span_n_log_n = span_calc(a, work_function_n_log_n, n)
+
+        print(f"n = {n}: Span (Constant Work) = {span_1}")
+        print(f"n = {n}: Span (Quadratic Work) = {span_n_squared}")
+        print(f"n = {n}: Span (Linearithmic Work) = {span_n_log_n}")
 
             # Add more test cases as needed
-        ]
+        n_values = [1, 10, 100, 1000, 10000]
+test_compare_span(n_values)
 
