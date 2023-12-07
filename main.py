970ade20d10cb179017d1285f82e8f725e8418cb
zdupre18 <126183811+zdupre18@users.noreply.github.com>
2023-12-07 11:29:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ def simple_work_calc(n, a, b):
 	Returns: the value of W(n).
 	"""
 	# TODO
-	 if n == 1:
-        return 1  # Base case
-
-    # Recursive step
-    return a * simple_work_calc(n // b, a, b) + n
-
+	if n == 1:
+		return 1
+	elif n == 0:
+		return 0
+	return a*simple_work_calc(n//b, a, b) + n
+def test_simple_work():
+	""" done. """
+	assert simple_work_calc(8, 2, 2) == 32
+	assert simple_work_calc(8, 3, 2) == 65  
+	assert simple_work_calc(9, 2, 3) == 19  
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
 
@@ -38,13 +42,10 @@ def work_calc(n, a, b, f):
 	"""
 	# TODO
 	if n == 1:
-        return f(1)  
-
-    total_work = f(n)  
-    for i in range(1, b + 1):
-        total_work += a * work_calc(n // b, a, b, f)
-
-    return total_work
+		return 1
+	elif n == 0:
+		return 0
+	return a*work_calc(n//b, a, b, f) + f(n)
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -63,13 +64,18 @@ def span_calc(n, a, b, f):
         return f(1)  # Base case
 
     # Recursive step
-    total_work = f(n)  # Work done at the current node
-    for i in range(1, b + 1):
-        total_work += a * work_calc(n // b, a, b, f)
+   if n == 1:
+		return 1
+	elif n == 0:
+		return 0
+	return span_calc(n//b, a, b, f) + f(n)
 
-    return total_work
 
-
+def test_work():
+	""" done. """
+	assert work_calc(8, 2, 2,lambda n: n) == 32 
+	assert work_calc(8, 1, 2, lambda n: n*n) == 85 
+	assert work_calc(8, 3, 2, lambda n: 1) == 40   
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
@@ -82,7 +88,7 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 	
 	"""
 	result = []
-	for n in input_sizes:
+	for n in sizes:
 		# compute W(n) using current a, b, f
 		result.append((
 			n,
@@ -90,6 +96,7 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 			work_fn2(n)
 			))
 	return result
+
 def span_calc(a, W, n):
 def work_function_constant(n):
     return 1
@@ -119,34 +126,34 @@ def work_fn2(n):
 def test_compare_work(n_values):
     for n in n_values:
         # Compare work functions using compare_work
-        work1 = work_calc(work_fn1, n)
-        work2 = work_calc(work_fn2, n)
+       def curry_work(a,b,f):
+		return lambda n: work_calc(n, a, b, f)
 
-        print(f"n = {n}: Work Function 1 = {work1}, Work Function 2 = {work2}")
+	res = compare_work(curry_work(2, 2, lambda n: 1),
+			  		   curry_work(2, 2, lambda n: n))
+	print_work_results(res)
+
+	res = compare_work(curry_work(2, 2, lambda n: n),
+			  		   curry_work(2, 2, lambda n: n*n))
+	print_work_results(res)
+
 	# curry work_calc to create multiple work
 	# functions taht can be passed to compare_work
     
 	# create work_fn1
 	# create work_fn2
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
-n_values = [1, 10, 100, 1000, 10000]
-test_compare_work(n_values)
-
+  
 def test_compare_span(n_values):
-    a = 2  # Number of processors
+   def curry_span(a,b,f):
+		return lambda n: span_calc(n, a, b, f)
 
-    for n in n_values:
-        span_1 = span_calc(a, work_function_1, n)
-        span_n_squared = span_calc(a, work_function_n_squared, n)
-        span_n_log_n = span_calc(a, work_function_n_log_n, n)
+	res = compare_span(curry_span(2, 2, lambda n: 1),
+			  		   curry_span(2, 2, lambda n: n))
+	print_span_results(res)
 
-        print(f"n = {n}: Span (Constant Work) = {span_1}")
-        print(f"n = {n}: Span (Quadratic Work) = {span_n_squared}")
-        print(f"n = {n}: Span (Linearithmic Work) = {span_n_log_n}")
+	res = compare_span(curry_span(2, 2, lambda n: n),
+			  		   curry_span(2, 2, lambda n: n*n))
+	print_span_results(res)
 
-            # Add more test cases as needed
-        n_values = [1, 10, 100, 1000, 10000]
-test_compare_span(n_values)
 
